b07aeb3fb2b40536c9e5f9691552012916755b12
Luis Ramirez <rluis4490@gmail.com>
2018-02-09 14:14:50 -0600

    una modificacion simple

--- USAC_WEB/src/usac_web/USAC_WEB.java
+++ USAC_WEB/src/usac_web/USAC_WEB.java
@@ -15,7 +15,7 @@ public class USAC_WEB {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        //Prueba de que todo funciona tbien
     }
     
 }
